eeb3d4a0240b2e0d4c73d5573846c08db08317e8
JuanLicciardi <juanelicciardi@gmail.com>
2022-11-29 22:03:18 -0300

    Finalizado microdesafio 2 // Fin TP

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -7,26 +7,73 @@ window.onload = function(){
     article.classList.add('fondoTransparente');
     formulario.classList.add('fondoCRUD');
 
+    let tittle = document.querySelector('#titulo');
 
+    let estado = 0
+    let estadoSecreto = 0
+    let vista = ""
+
+    
     function generarNuevoColor(){                           // No es de mi propiedad pero me parecio buena usarla
         let simbolos, color;
         simbolos = "0123456789ABCDEF";
         color = "#";
         for(let i = 0; i < 6; i++){
             color = color + simbolos[Math.floor(Math.random() * 16)];
-            console.log(color)
         }
         return color
     }
     
-
+    tittle.focus()
     titulo.addEventListener('mouseover', function(){
         titulo.style.color = generarNuevoColor()
     })
 
+    tittle.addEventListener('keyup',async function(event){
+        if (event.key == "s" && estado == 0){
+            estadoSecreto += 1
+            estado ++
+            vista += "*"
+        }else if(event.key == "e" && estado == 1){
+            estadoSecreto += 1
+            estado ++
+            vista += "*"
+        }else if(event.key == "c" && estado == 2){
+            estadoSecreto += 1
+            estado ++
+            vista += "*"
+        }else if(event.key == "r" && estado == 3){
+            estadoSecreto += 1
+            estado ++
+            vista += "*"
+        }else if(event.key == "e" && estado == 4){
+            estadoSecreto += 1
+            estado ++
+            vista += "*"
+        }else if(event.key == "t" && estado == 5){
+            estadoSecreto += 1
+            estado ++
+            vista += "*"
+        }else if(event.key == "o" && estado == 6){
+            estadoSecreto += 1
+            estado ++
+            vista += "*"
+        }else {
+            estadoSecreto = 0
+            estado = 0
+            vista = ""
+        }
 
+        if (estadoSecreto > 6 ){
+            vista = "SECRETO MAGICO"
+            estadoSecreto = 0
+            estado = 0
+            setTimeout(function(){
+                vista=""
+                tittle.value = ""
+            }, 5000);
 
-
-
-
+        }
+        tittle.value = vista 
+    })
 }
